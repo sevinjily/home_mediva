6a7a214c18e2edc9ce6ed0169c9b0650f01c1ec6
svncmsva <sevincmusaeva01@gmail.com>
2023-11-19 00:44:01 +0400

    new section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+"use strict";
 // Header start
 // ------------------
 
@@ -8,3 +9,19 @@
 
 
 // Header finish
+const next=document.getElementsByClassName("slider next");
+const prev=document.getElementsByClassName("slider prev");
+const category=document.querySelectorAll("slider","sliderCategory");
+let current=0;
+next.addEventListener("click",function(){
+
+
+if(current<slider.length-1){
+    current+=1;
+}
+else{
+    current=0;
+}
+updateSlider();
+
+})
