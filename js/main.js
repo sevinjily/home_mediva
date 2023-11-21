e2b0aeb86d2dbc5295aa11539cde237a6a9bf83b
svncmsva <sevincmusaeva01@gmail.com>
2023-11-21 22:23:54 +0400

    other section

--- js/main.js
+++ js/main.js
@@ -2,26 +2,34 @@
 // Header start
 // ------------------
 
-
-
-
-
-
-
 // Header finish
-const next=document.getElementsByClassName("slider next");
-const prev=document.getElementsByClassName("slider prev");
-const category=document.querySelectorAll("slider","sliderCategory");
-let current=0;
-next.addEventListener("click",function(){
-
-
-if(current<slider.length-1){
-    current+=1;
-}
-else{
+const next = document.querySelector(".slider .next");
+const prev = document.querySelector(".slider .prev");
+// const category = document.querySelectorAll(".slider .  sliderCategory .category");
+const slider = document.querySelector(".slider .sliderCategory");
+let current = 0;
+next.addEventListener("click", function () {
+  if(current<slider.children.length-1){
+    current++;
+  }
+  else{
     current=0;
-}
-updateSlider();
+  }
+  updateSlider();
 
-})
+});
+prev.addEventListener("click",function(){
+    if(current>0){
+        current--;
+    }
+    else{
+        current=slider.children.length-1;
+    }
+    updateSlider();
+
+});
+function updateSlider() {
+    const translateValue = -current * 100 + '%';
+    slider.style.transform = 'translateX('+ translateValue + ')';
+  }
+;
